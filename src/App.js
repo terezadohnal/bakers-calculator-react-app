80ffe072a050ab9a909214685bfd0f9e07f47d2d
samueldusek <smldsk@protonmail.com>
2021-08-30 19:16:20 +0200

    add Layout component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Layout />
       <Footer />
     </div>
   );
