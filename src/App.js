dd271dcdebd3523986ea7c90c1fbaf0baf83d02b
Tereza Dohnalova <xdohna10@mendelu.cz>
2021-08-30 19:42:19 +0200

    Add Footer styles and Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Layout from "./components/Layout/Layout";
+import classes from "./App.module.css";
 
 function App() {
   return (
-    <div>
+    <div className={classes.wrapper}>
       <Navbar />
       <Layout />
       <Footer />
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import classes from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <p>Made by Tereza & Samuel</p>
+      <p>Made by <a href="https://github.com/terezadohnal">Tereza</a> & <a href="https://github.com/samueldusek">Samuel</a></p>
     </footer>
   );
 };
